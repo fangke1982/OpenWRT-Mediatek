b8a4137c90cdc56c6be470ed3beeeacc95e15534
NERD <168670760+zhiern@users.noreply.github.com>
2025-07-15 00:42:44 +0800

    Update 00-prepare_base.sh

--- openwrt/scripts/00-prepare_base.sh
+++ openwrt/scripts/00-prepare_base.sh
@@ -86,7 +86,7 @@ git clone https://$gitea/packages_utils_runc feeds/packages/utils/runc
 # SSRP & Passwall
 rm -rf feeds/luci/applications/{luci-app-daed,luci-app-dae,luci-app-homeproxy,luci-app-openclash,luci-app-passwall}
 rm -rf feeds/packages/net/{daed,xray-core,v2ray-core,v2ray-geodata,sing-box}
-git clone -b openwrt-24.10 https://$gitea/openwrt_helloworld package/new/helloworld
+git clone https://"$git_name":"$git_password"@$gitea/openwrt_helloworld package/new/helloworld
 
 # argon
 rm -rf feeds/luci/themes/luci-theme-argon
