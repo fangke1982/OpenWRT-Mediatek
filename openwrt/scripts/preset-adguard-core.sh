a09cee0662319cbdfd6007fafbbf0ed3c2278d79
NERD <168670760+zhiern@users.noreply.github.com>
2025-07-14 22:43:25 +0800

    Update preset-adguard-core.sh

--- openwrt/scripts/preset-adguard-core.sh
+++ openwrt/scripts/preset-adguard-core.sh
@@ -2,7 +2,7 @@
 
 mkdir -p files/usr/bin
 
-AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_${1} | awk -F '"' '{print $4}')
+AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_arm64 | awk -F '"' '{print $4}')
 
 wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome
 
