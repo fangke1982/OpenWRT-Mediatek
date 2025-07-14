07a48c1344abd20e252dcf5f33958a436fd4dcd8
NERD <168670760+zhiern@users.noreply.github.com>
2025-07-14 22:14:39 +0800

    Update prepare_base.sh

--- openwrt/scripts/prepare_base.sh
+++ openwrt/scripts/prepare_base.sh
@@ -25,7 +25,11 @@ sed -i "s/ImmortalWrt-2.4G/$Wifi_Name-2.4G/g" package/mtk/applications/mtwifi-cf
 sed -i "s/ImmortalWrt-5G/$Wifi_Name-5G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
 # default wifi password
-
+sed -i '/^[[:space:]]*set wireless\.default_.*\.encryption=none/ {
+s/=none/=psk2+ccmp/
+a\
+					set wireless.default_${dev}.key=$Wifi_Password
+}' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
 # Version settings
 cat << 'EOF' >> feeds/luci/modules/luci-mod-status/ucode/template/admin_status/index.ut
