e4f4e8468802fe454710c0a96d5c0219770dce47
NERD <168670760+zhiern@users.noreply.github.com>
2025-07-17 10:05:04 +0800

    Update 01-prepare_package.sh

--- openwrt/scripts/01-prepare_package.sh
+++ openwrt/scripts/01-prepare_package.sh
@@ -21,7 +21,6 @@ git clone https://$gitea/packages_utils_runc feeds/packages/utils/runc
 rm -rf feeds/luci/applications/{luci-app-daed,luci-app-dae,luci-app-homeproxy,luci-app-openclash,luci-app-passwall}
 rm -rf feeds/packages/net/{daed,xray-core,v2ray-core,v2ray-geodata,sing-box}
 git clone https://"$git_name":"$git_password"@$gitea/openwrt_helloworld package/new/helloworld
-rm -rf package/new/helloworld/{daed,luci-app-daed,nikki,luci-app-nikki,dns2socks-rust,luci-app-homeproxy}
 
 # argon
 rm -rf feeds/luci/themes/luci-theme-argon
