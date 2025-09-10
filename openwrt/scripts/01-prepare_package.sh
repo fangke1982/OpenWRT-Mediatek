64e39f54c4c0305a9b7bec6e5066b7d0d467f665
fangke1982 <36264415+fangke1982@users.noreply.github.com>
2025-09-10 14:39:42 +0800

    Update 01-prepare_package.sh

--- openwrt/scripts/01-prepare_package.sh
+++ openwrt/scripts/01-prepare_package.sh
@@ -21,7 +21,7 @@ git clone https://$gitea/packages_utils_runc feeds/packages/utils/runc
 # SSRP & Passwall
 rm -rf feeds/luci/applications/{luci-app-daed,luci-app-dae,luci-app-homeproxy,luci-app-openclash,luci-app-passwall}
 rm -rf feeds/packages/net/{daed,xray-core,v2ray-core,v2ray-geodata,sing-box}
-git clone https://"$git_name":"$git_password"@$gitea/openwrt_helloworld package/new/helloworld
+git clone https://github.com/sbwml/openwrt_helloworld
 
 # argon
 rm -rf feeds/luci/themes/luci-theme-argon
