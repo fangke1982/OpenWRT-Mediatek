e9f5e8da51ee3cfb04c7e285b2dcc9ccb8dc648c
NeonPulse-Zero <3922462371@qq.com>
2025-09-16 10:53:04 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -13,6 +13,16 @@ sed -i 's/LEDE/ZeroWrt/' package/base-files/files/bin/config_generate
 # TTYD 免登录
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
 # 移除要替换的包
 rm -rf feeds/packages/net/{mosdns,pdnsd-alt,xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 rm -rf feeds/luci/applications/{luci-app-mosdns,luci-app-netdata,luci-app-serverchan}
@@ -23,13 +33,13 @@ rm -rf feeds/packages/lang/golang
 git clone $mirror/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
 # 添加额外插件
-git clone --depth=1 https://$github/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-git clone --depth=1 -b openwrt-18.06 https://$github/tty228/luci-app-wechatpush package/luci-app-serverchan
-git clone --depth=1 https://$github/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
-git clone --depth=1 https://$github/esirplayground/luci-app-poweroff package/luci-app-poweroff
-git clone --depth=1 https://$github/destan19/OpenAppFilter package/OpenAppFilter
-git clone --depth=1 https://$github/Jason6111/luci-app-netdata package/luci-app-netdata
-git_sparse_clone main https://$github/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
+git clone --depth=1 https://$github/kongfl888/luci-app-adguardhome package/new/luci-app-adguardhome
+git clone --depth=1 -b openwrt-18.06 https://$github/tty228/luci-app-wechatpush package/new/luci-app-serverchan
+git clone --depth=1 https://$github/ilxp/luci-app-ikoolproxy package/new/luci-app-ikoolproxy
+git clone --depth=1 https://$github/esirplayground/luci-app-poweroff package/new/luci-app-poweroff
+git clone --depth=1 https://$github/destan19/OpenAppFilter package/new/OpenAppFilter
+git clone --depth=1 https://$github/Jason6111/luci-app-netdata package/new/luci-app-netdata
+git_sparse_clone main https://$github/Lienol/openwrt-package new/luci-app-filebrowser new/luci-app-ssr-mudb-server
 git_sparse_clone openwrt-18.06 https://$github/immortalwrt/luci applications/luci-app-eqos
 
 # 科学上网插件
