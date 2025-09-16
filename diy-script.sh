e23a819de602d1874445f9cccf773170e8e885ea
NeonPulse-Zero <3922462371@qq.com>
2025-09-16 10:43:12 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# 设置变量
+export github="github.com"
+export mirror="http://10.0.0.245:3000/zhao"
+
 # 修改默认IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
@@ -16,28 +20,28 @@ rm -rf feeds/luci/themes/{luci-theme-argon,luci-theme-netgear}
 
 # golang 1.25
 rm -rf feeds/packages/lang/golang
-git clone http://10.0.0.245:3000/zhao/packages_lang_golang -b 25.x feeds/packages/lang/golang
+git clone $mirror/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
 # 添加额外插件
-git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
-git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
-git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
-git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
-git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
-git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
+git clone --depth=1 https://$github/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+git clone --depth=1 -b openwrt-18.06 https://$github/tty228/luci-app-wechatpush package/luci-app-serverchan
+git clone --depth=1 https://$github/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
+git clone --depth=1 https://$github/esirplayground/luci-app-poweroff package/luci-app-poweroff
+git clone --depth=1 https://$github/destan19/OpenAppFilter package/OpenAppFilter
+git clone --depth=1 https://$github/Jason6111/luci-app-netdata package/luci-app-netdata
+git_sparse_clone main https://$github/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
+git_sparse_clone openwrt-18.06 https://$github/immortalwrt/luci applications/luci-app-eqos
 
 # 科学上网插件
-git clone --depth=1 http://10.0.0.245:3000/zhao/openwrt_helloworld package/helloworld
+git clone --depth=1 $mirror/openwrt_helloworld package/helloworld
 
 # Themes
-git clone --depth=1 -b 18.06 http://10.0.0.245:3000/zhao/luci-theme-argon package/new/luci-theme-argon
-git clone --depth=1 -b 18.06 http://10.0.0.245:3000/zhao/luci-app-argon-config package/new/luci-app-argon-config
+git clone --depth=1 -b 18.06 $mirror/luci-theme-argon package/new/luci-theme-argon
+git clone --depth=1 -b 18.06 $mirror/luci-app-argon-config package/new/luci-app-argon-config
 
 # MosDNS
-git clone --depth=1 http://10.0.0.245:3000/zhao/v2ray-geodata package/v2ray-geodata
-git clone --depth=1 -b v5-lua http://10.0.0.245:3000/zhao/luci-app-mosdns package/new/luci-app-mosdns
+git clone --depth=1 $mirror/v2ray-geodata package/v2ray-geodata
+git clone --depth=1 -b v5-lua $mirror/luci-app-mosdns package/new/luci-app-mosdns
 
 # 默认设置
 cp -f $GITHUB_WORKSPACE/scripts/zzz-default-settings package/lean/default-settings/files/zzz-default-settings
