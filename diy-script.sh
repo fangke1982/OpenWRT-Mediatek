730f6a52e18bd3fe77663b997e37d39e35dfd56c
NeonPulse-Zero <3922462371@qq.com>
2025-09-16 17:00:07 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -13,16 +13,6 @@ sed -i 's/LEDE/ZeroWrt/' package/base-files/files/bin/config_generate
 # TTYD 免登录
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
-# Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
-
 # 移除要替换的包
 rm -rf feeds/packages/net/{lucky,mosdns,pdnsd-alt,xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 rm -rf feeds/luci/applications/{luci-app-mosdns,luci-app-netdata,luci-app-serverchan,luci-app-lucky}
@@ -34,10 +24,6 @@ git clone $mirror/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
 # 更改 Argon 主题背景
 cp -f $GITHUB_WORKSPACE/other/bg1.jpg feeds/Zero/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-
-# MosDNS
-git clone --depth=1 $mirror/v2ray-geodata package/v2ray-geodata
-git clone --depth=1 -b v5-lua $mirror/luci-app-mosdns package/luci-app-mosdns
 
 # 默认设置
 cp -f $GITHUB_WORKSPACE/other/zzz-default-settings package/lean/default-settings/files/zzz-default-settings
