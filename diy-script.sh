009c6b71b626d423b4665ce2bd77d6d0aa418e94
NeonPulse-Zero <3922462371@qq.com>
2025-09-18 21:50:14 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -10,7 +10,7 @@ sed -i 's/LEDE/ZeroWrt/' package/base-files/files/bin/config_generate
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 移除要替换的包
-rm -rf feeds/packages/net/{lucky,mosdns,pdnsd-alt,xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+rm -rf feeds/packages/net/{open-app-filter,lucky,mosdns,pdnsd-alt,xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 rm -rf feeds/luci/applications/{luci-app-argon-config,luci-app-mosdns,luci-app-netdata,luci-app-serverchan,luci-app-lucky}
 rm -rf feeds/luci/themes/{luci-theme-argon,luci-theme-netgear}
 
